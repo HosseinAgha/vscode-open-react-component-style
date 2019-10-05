48feb297d72cf818233fb7881a4eca3136ca8056
Hossein Saniei <h.saniei@gmail.com>
2019-10-05 19:10:47 +0330

    feat: add semantic-release and ci-cd

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  verifyConditions: ['semantic-release-vsce', '@semantic-release/github'],
+  prepare: {
+    path: 'semantic-release-vsce',
+    packageVsix: 'vscode-open-react-component-style.vsix',
+  },
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    'semantic-release-vsce',
+    {
+      path: '@semantic-release/github',
+      assets: 'vscode-open-react-component-style.vsix',
+    },
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
